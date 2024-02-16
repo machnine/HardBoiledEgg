6486f925d3ace90dc52aeb2424a0d7ab2372627a
Mian Chen <mianchen@gmail.com>
2024-02-16 16:14:33 +0000

    updated Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -6,12 +6,6 @@ WORKDIR /app
 COPY . /app
 # install dependencies
 RUN pip install --upgrade pip && pip install -r requirements.txt
-# check if the code is working
-RUN python manage.py check
-# set dummy variables only for building the image
-ENV SECRET_KEY="dummy_secret_key"
-ENV DJANGO_SETTINGS_MODULE="hardboiledegg.settings.production"
-# collect static files
-RUN python manage.py collectstatic --noinput
+# run the entrypoint.sh
 RUN chmod +x /app/entrypoint.sh
 CMD ["/app/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,19 @@
 #!/bin/sh
 
 # database migrations
+echo "Apply database migrations..."
 python manage.py migrate 
 
+# running Django check
+echo "Running Django check..."
+python manage.py check
+
+# collect static files
+echo "Collect static files..."
+python manage.py collectstatic --noinput
+
 # create superuser
+echo "Creating superuser..."
 python manage.py createadmin
 
 # Start Gunicorn processes
@@ -11,6 +21,5 @@ echo Starting Gunicorn...
 exec gunicorn hardboiledegg.wsgi:application \
     --bind "0.0.0.0:8000" \
     --workers 3 \
-    --timeout 300 \
-    
+    --timeout 300
     
